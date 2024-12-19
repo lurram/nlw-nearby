c3c7dce9cfaf3c67bdddf5a709c70488e877e211
lurram <lurramsantos.00.ls@gmail.com>
2024-12-19 17:54:54 -0300

    complete code

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,38 @@
+import { colors } from "@/styles/colors"
+import { Stack } from "expo-router"
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+import { 
+  useFonts, 
+  Rubik_400Regular, 
+  Rubik_500Medium, 
+  Rubik_600SemiBold, 
+  Rubik_700Bold 
+} from "@expo-google-fonts/rubik"
+
+import { Loading } from "@/components/loading"
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_400Regular, 
+    Rubik_500Medium, 
+    Rubik_600SemiBold, 
+    Rubik_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack 
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {  
+            backgroundColor: colors.gray[100],}
+          }}
+      />
+    </GestureHandlerRootView>
+  )
+}
